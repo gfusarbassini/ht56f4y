941fce958000d28f370e0dcc7f680531199ffb8a
Roberto Ciccarelli <robyciccarelli.rc@gmail.com>
2022-12-31 19:19:46 +0100

    update home

--- js/main.js
+++ js/main.js
@@ -16,4 +16,53 @@ $(document).ready(function () {
     }
   );
 
+
+  $("html").on("click", ".card", function(evt) {
+    var btn = $(evt.currentTarget);
+    var x = evt.pageX - btn.offset().left;
+    var y = evt.pageY - btn.offset().top;
+
+    var duration = 1000;
+    var animationFrame, animationStart;
+    var colorString = btn.css("background-color");
+    if (colorString.indexOf('rgba') === -1)
+      colorString += ',1';
+    var startingColor =  colorString.match(/[\.\d]+/g).map(function (a)
+        {
+            return +a*0.45
+    });
+    startingColorString = startingColor[0] + "," + startingColor[1] + "," + startingColor[2];
+    console.log(startingColor);
+    var animationStep = function(timestamp) {
+      if (!animationStart) {
+        animationStart = timestamp;
+      }
+
+      var frame = timestamp - animationStart;
+      if (frame < duration) {
+        var easing = (frame/duration) * (2 - (frame/duration));
+
+        var circle = "circle at " + x + "px " + y + "px";
+
+        var color = "rgba(" + startingColorString + ", " + (0.3 * (1 - easing)) + ")";
+        console.log(color);
+        var stop = 90 * easing + "%";
+
+        btn.css({
+          "background-image": "radial-gradient(" + circle + ", " + color + " " + stop + ", transparent " + stop + ")"
+        });
+
+        animationFrame = window.requestAnimationFrame(animationStep);
+      } else {
+        $(btn).css({
+          "background-image": "none"
+        });
+        window.cancelAnimationFrame(animationFrame);
+      }
+
+    };
+
+    animationFrame = window.requestAnimationFrame(animationStep);
+
+  });
 });
