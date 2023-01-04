332fdf7e6d06fe36d28186c8e01c1f81742419b7
Roberto Ciccarelli <robyciccarelli.rc@gmail.com>
2023-01-05 00:50:53 +0100

    Speakers /2

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 
 
 
-  $("html").on("click", ".card, .ripple", function(evt) {
+  $("html").on("click", ".ripple", function(evt) {
     var btn = $(evt.currentTarget);
     var x = evt.pageX - btn.offset().left;
     var y = evt.pageY - btn.offset().top;
@@ -113,8 +113,82 @@ $(document).ready(function () {
     $(".overlay").fadeIn();
   });
 
+
   $(".nav-close, .overlay").on("click", function () {
-    $(".sidenav").css({ "left": "-100vw" });
-    $(".overlay").fadeOut();
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
+    if (mediaQuery.matches) {
+      $(".sidenav").css({ "left": "-100vw" });
+      $(".overlay").fadeOut();
+    }
   });
+
+  $("html").on("click", ".overlay, .trigger-close", function () {
+    const mediaQuery = window.matchMedia('(min-width: 600px)');
+
+    $(".card-content").fadeOut(300);
+    $(".overlay").fadeOut(300);
+    $(".card.expandable.opened").removeClass("shadowed");
+    $(".card-action-top.trigger-close i").html("more_vert").parent().removeClass("trigger-close");
+
+    $(".card.expandable.opened").animate({
+      "width": $(".card.expandable.opened").data("width"),
+      "height": $(".card.expandable.opened").data("height"),
+      "left": $(".card.expandable.opened").data("left"),
+      "top": $(".card.expandable.opened").data("top"),
+    }, 500, $.easie(0.05, 0.1, 0.1, 1.0), function () {
+
+      $(".card.expandable.opened").css({
+        "left": 0,
+        "top": 0,
+        "height": "",
+        "width": "",
+        "justify-content": "",
+        "margin": ""
+      });
+      $(".card.expandable").removeClass("opened");
+
+    });
+
+  });
+
+  $("html").on("click", ".card.expandable:not(.opened)", function () {
+    //if ($(this).hasClass("opened")) return;
+    console.log("ciao");
+    let top = $(this).offset().top;
+    let left = $(this).offset().left;
+    let width = $(this).outerWidth();
+    let height = $(this).outerHeight();
+    $(this).addClass("opened shadowed");
+    $(this).css({ "top": top, "left": left, "width": width, "height": height, "justify-content": "flex-start" })
+      .attr("data-top", top)
+      .attr("data-left", left)
+      .attr("data-width", width)
+      .attr("data-height", height).removeClass("ripple");
+    $(".overlay").fadeIn();
+    $(".card-content", this).delay(300).fadeIn();
+
+    const mediaQuery = window.matchMedia('(min-width: 600px)');
+    if (mediaQuery.matches) {
+
+      $(this).animate({
+        "width": "50vw",
+        "height": "50vh",
+        "left": "25vw",
+        "top": "25vh"
+      }, 500, $.easie(0.05, 0.1, 0.1, 1.0));
+
+    } else {
+      $(this).parent().css("height", height);
+      $(this).animate({
+        "width": "100vw",
+        "height": "100vh",
+        "margin": "0px",
+        "left": "0px",
+        "top": "0px"
+      }, 500, $.easie(0.05, 0.1, 0.1, 1.0));
+    }
+
+    $(".card-action-top i", this).html("close").parent().addClass("trigger-close");
+  });
+
 });
